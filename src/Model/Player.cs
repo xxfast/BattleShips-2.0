847dbda3f5f946ec533268bf02751c3f14c3ffe5
Shameera <100047971@student.swin.edu.au>
2015-10-08 10:33:21 +1100

    Fix invalid hits bug
    Fixed it so that it does not record when it hits multiple times on the
    same box

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -179,7 +179,6 @@ public class Player : IEnumerable<Ship>
 	/// <returns>the result of the attack</returns>
 	internal AttackResult Shoot(int row, int col)
 	{
-		_shots += 1;
 		AttackResult result = default(AttackResult);
 		result = EnemyGrid.HitTile(row, col);
 
@@ -187,9 +186,13 @@ public class Player : IEnumerable<Ship>
 			case ResultOfAttack.Destroyed:
 			case ResultOfAttack.Hit:
 				_hits += 1;
+				_shots += 1;
+
 				break;
 			case ResultOfAttack.Miss:
 				_misses += 1;
+				_shots += 1;
+
 				break;
 		}
 
